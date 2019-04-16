a32b1379562918e73b60dfe9fae0a226645bb0e9
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-04-16 16:12:13 -0600

    debugging

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/css', express.static('css'));
 
-
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs');
+});
 
 app.get('/pokemon', (req,res) => {
     res.render('index.ejs', {pokemon: pokemon})
@@ -18,9 +20,28 @@ app.get('/pokemon/:id', (req,res) =>{
     res.render('show.ejs', {pokemon: pokemon[req.params.id]}); 
 });
 
+
 app.post('/pokemon', (req, res) => {
     res.redirect('/pokemon');
 });
+
+app.put('/pokemon', (req, res) => {
+    res.render('edit.ejs', {pokemon: pokemon[req.params.id], id: req.params.id});
+})
+
+app.put('/pokemon', (req, res) => {
+    pokemon[req.params.id] = req.body;
+    res.redirect('/pokemon')
+});
+
+app.delete('/pokemon/:id', (req, res) => {
+    pokemon.splice(req.params.id, 1);
+    res.redirect('/pokemon');
+});
+
+
+
+
 
 
 
